193c2d6b41c15d0881f7a29784622e4ca28ff65b
Chris Stephenson <clstephenson@gmail.com>
2019-06-24 17:37:50 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+
+This is a personal website and blog using Gatsby.
+
+The original starter used for this site was [gatsby-starter-lumen](https://github.com/alxshelepenok/gatsby-starter-lumen), which was created under the MIT license with the following copyright: Copyright (c) 2016-2018 Alexander Shelepenok.
+
---
+++ config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  url: "https://clstephenson.com",
+  title: "Blog by Chris Stephenson",
+  subtitle: "",
+  copyright: "©2019 All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "",
+  menu: [
+    {
+      label: "Articles",
+      path: "/"
+    },
+    {
+      label: "About me",
+      path: "/pages/about"
+    },
+    {
+      label: "Contact me",
+      path: "/pages/contacts"
+    }
+  ],
+  author: {
+    name: "Chris Stephenson",
+    photo: "/profile-pic.jpg",
+    bio:
+      "Hi! I'm a software developer who made a career switch after working as a field service engineer for a large manufacturer of medical devices. My current interests are in Java and Spring Boot. I'd like for this blog to be a place where I can document of some of the things that I learn and maybe help others along the way.",
+    contacts: {
+      email: "clstephenson@gmail.com",
+      linkedin: "stephensoncl",
+      github: "clstephenson",
+      twitter: "cl_stephenson",
+      rss: ""
+    }
+  }
+};
